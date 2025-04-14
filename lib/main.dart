632653be2292f2e94e61c5a58a1a218eecafab5a
Martin Bascuñan <martin.bascunan.g@gmail.com>
2025-04-14 19:04:59 -0400

    Update main.dart own Theme
    Punto 4-a Laboratorio 4 creando un propio "thema" con google fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,6 +14,30 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        // Define the default brightness and colors.
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.purple,
+          // ···
+          brightness: Brightness.dark,
+        ),
+
+        textTheme: TextTheme(
+          displayLarge: const TextStyle(
+          fontSize: 72,
+          fontWeight: FontWeight.bold,
+        ),
+
+        titleLarge: GoogleFonts.oswald(
+          fontSize: 30,
+          fontStyle: FontStyle.italic,
+        ),
+
+        bodyMedium: GoogleFonts.merriweather(),
+        displaySmall: GoogleFonts.pacifico(),
+      ),
+      ),
+      home: const MyHomePage(title:'Flutter Demo Home Page'),
+);
         // This is the theme of your application.
         //
         // TRY THIS: Try running your application with "flutter run". You'll see
@@ -28,10 +53,10 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        /*colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    );*/
   }
 }
 
